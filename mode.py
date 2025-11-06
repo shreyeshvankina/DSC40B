ba7d896f9121aad93a6db89580bc34be385e5d50
shreyeshvankina <shreyeshvankina@gmail.com>
2025-11-05 18:41:09 -0800

    Add files via upload

--- mode.py
+++ mode.py
@@ -1,1 +1,13 @@
+def mode(numbers):
+	occurences = {}
+	for num in numbers:
+		if num in occurences:
+			occurences[num] += 1
+		else:
+			occurences[num] = 1
 
+	maximum = (None, 0)
+	for key, value in occurences.items():
+		if (value > maximum[1]):
+			maximum = (key, value)
+	return maximum[0]
